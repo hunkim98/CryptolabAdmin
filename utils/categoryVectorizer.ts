21251ff1dc545ad572443ff10a51d0e3a082ab18
Kim Dong Hun <hunkim98@gmail.com>
2023-06-09 00:08:35 +0900

    Change Category ids

--- utils/categoryVectorizer.ts
+++ utils/categoryVectorizer.ts
@@ -1,27 +1,27 @@
 export type CategryText =
   | "Improper use of your report"
+  | "Getting a credit card"
+  | "Took or threatened to take negative or legal action"
   | "Trouble during payment process"
   | "Problem with a purchase shown on your statement"
-  | "Took or threatened to take negative or legal action"
   | "Struggling to pay mortgage"
-  | "Closing an account"
-  | "Getting a credit card";
+  | "Closing an account";
 
-export const returnCategoryFromIndex = (index: number) => {
+export const returnCategoryFromIndex = (index: number): CategryText => {
   if (index === 2) {
-    return "Improper use of your report" as CategryText;
+    return "Improper use of your report";
   } else if (index === 3) {
-    return "Trouble during payment process" as CategryText;
+    return "Getting a credit card";
   } else if (index === 4) {
-    return "Problem with a purchase shown on your statement" as CategryText;
+    return "Took or threatened to take negative or legal action";
   } else if (index === 5) {
-    return "Took or threatened to take negative or legal action" as CategryText;
+    return "Trouble during payment process";
   } else if (index === 6) {
-    return "Struggling to pay mortgage" as CategryText;
+    return "Problem with a purchase shown on your statement";
   } else if (index === 7) {
-    return "Closing an account" as CategryText;
+    return "Struggling to pay mortgage";
   } else if (index === 8) {
-    return "Getting a credit card" as CategryText;
+    return "Closing an account";
   } else {
     throw new Error("Invalid index");
   }
